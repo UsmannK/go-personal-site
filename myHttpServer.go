5270c31f2d440ef72e2903d991e0861342a2a485
UsmannK <usmann96@gmail.com>
2016-02-01 14:14:01 -0500

    Add iswavesout support

--- myHttpServer.go
+++ myHttpServer.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
+	"time"
+	"strings"
 	"fmt"
 )
 
-func servePersonalSite(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w,r,"static/" + r.URL.Path[1:])
+type Page struct {
+	Answer string
+	Body []byte
 }
 
-func serveKanyeSite(w httpResponseWriter, r *http.Request) {
-	http.ServeFile(w,r,"kanye/")
+func servePersonalSite(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w,r,"static/" + r.URL.Path[1:])	
+}
+
+func serveKanyeSite(w http.ResponseWriter, r *http.Request) {
+	if !strings.Contains(r.URL.Path, "static") {
+		answer := "NO"
+		if isWaves() {
+			answer = "YES"
+		}
+		p := &Page{Answer: answer}
+		t, _ := template.ParseFiles("kanye/index.html")
+		t.Execute(w, p)
+	} else {
+			fmt.Print("static/" + r.URL.Path[8:])
+			http.ServeFile(w,r,"static/" + r.URL.Path[8:])	
+	}
 }
 
 var mux map[string]func(http.ResponseWriter, *http.Request)
@@ -27,10 +46,15 @@ func main() {
 type myHandler struct{}
 
 func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if string.Contains(string,ToLower(r.Host), "iswavesoutyet.com") {
-		serveKanye(w,r)
-	}
-	else {
+	if strings.Contains(strings.ToLower(r.Host), "iswavesoutyet.com") {
+		serveKanyeSite(w,r)
+	} else {
 		servePersonalSite(w,r)
 	}
+}
+
+func isWaves() (bool) {
+	waves,_ := time.Parse(time.RFC822, "11 Feb 16 00:00")
+	now := time.Now()
+	return waves.After(now)
 }
